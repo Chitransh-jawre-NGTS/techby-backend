1e49702d68ee13fb8effbe792c42f5bf5b4a6af8
chitransh <chitranshjawre2001@gmail.com>
2026-05-13 01:27:18 +0530

    update

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ dotenv.config();
 // ✅ Connect Database
 connectDB();
 
-dotenv.config();
-
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -60,6 +58,10 @@ cron.schedule("0 0 * * *", () => console.log("🕒 Daily cron job executed"));
 
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
 const server = http.createServer(app);
-server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
