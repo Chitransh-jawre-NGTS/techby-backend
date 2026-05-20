b31693a9924ebdac58db55eb35b95c2f541ce933
chitransh <chitranshjawre2001@gmail.com>
2026-05-20 15:03:04 +0530

    remove firebase credentials

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,26 @@
+// const admin = require("firebase-admin");
+
+// const serviceAccount = require("../firebase-service-account.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(
+//     serviceAccount
+//   ),
+// });
+
+// module.exports = admin; 
+
+
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../firebase-service-account.json");
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
-admin.initializeApp({
-  credential: admin.credential.cert(
-    serviceAccount
-  ),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
